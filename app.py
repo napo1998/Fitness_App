3358ed81d1c2878e9add2642d27f6cbc6a26f36a
napo1998 <129983892+napo1998@users.noreply.github.com>
2026-01-05 07:22:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -797,6 +797,6 @@ with tab5:
 st.markdown("---")
 st.markdown("""
     <div style='text-align: center; color: gray;'>
-    <small>❄️ Winter Arc Challenge 2025 |Developed by Napoleon Perez & Arturo Boquins | Transform this winter 💪</small>
+    <small>❄️ Winter Arc Challenge 2025 |Developed by Napoleon Perez & Arturo Boquin | Transform this winter 💪</small>
     </div>
 """, unsafe_allow_html=True)
